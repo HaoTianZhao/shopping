6ea5dde6e0572dfb8d9eec954146df44af427523
Zhao <597611084@qq.com>
2019-03-26 19:05:44 +0800

    project init commit

---
+++ src/main/java/com/shopping/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.shopping.dao;
+
+import com.shopping.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/com/shopping/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.shopping.dao;
+
+import com.shopping.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
